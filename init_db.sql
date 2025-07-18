f8acf510dda7ccb9b1d86b9e2228f19ad0cd073f
clement-leroy-mps <Clement.Leroy@monolithicpower.com>
2025-07-18 16:03:42 +0200

    Update init_db.sql

--- init_db.sql
+++ init_db.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `project_description`
 --
 
+USE emc_lab_project;
+
 DROP TABLE IF EXISTS `project_description`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
